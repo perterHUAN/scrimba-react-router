b34ab91d32a4c6f6813ccae9d56ab399c6f55b05
perterHUAN <47806541+perterHUAN@users.noreply.github.com>
2024-04-06 20:22:06 +0800

    Introduction to react router: Link

--- index.jsx
+++ index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDom from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 function App() {
   return <h1>Hello World</h1>;
 }
@@ -10,6 +10,10 @@ function About() {
 
 ReactDom.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+    </nav>
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/about" element={<About />} />
