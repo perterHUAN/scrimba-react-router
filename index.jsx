06c3ff119c2bb7c6eb16490ef245b63fca430147
perterHUAN <47806541+perterHUAN@users.noreply.github.com>
2024-04-09 17:31:03 +0800

    Set up tailwindcss and add initial styles

--- index.jsx
+++ index.jsx
@@ -1,18 +1,29 @@
 import React from "react";
 import ReactDom from "react-dom/client";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import "./index.css";
 function App() {
-  return <h1>Hello World</h1>;
+  return <h1 class="mt-2">Hello World</h1>;
 }
 function About() {
-  return <h1>About page goes here🍕</h1>;
+  return <h1 class="mt-2">About page goes here🍕</h1>;
 }
 
 ReactDom.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
+    <nav className="flex gap-2 py-2 pl-3">
+      <Link
+        to="/"
+        className="px-4 py-2 rounded bg-orange-300 font-bold hover:bg-orange-50"
+      >
+        Home
+      </Link>
+      <Link
+        to="/about"
+        className="px-4 py-2 rounded bg-orange-300 font-bold hover:bg-orange-50"
+      >
+        About
+      </Link>
     </nav>
     <Routes>
       <Route path="/" element={<App />} />
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,css,jsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
