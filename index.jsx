132132d0f6c02d98c7fa142555400d519fed7266
perterHUAN <47806541+perterHUAN@users.noreply.github.com>
2024-04-10 16:20:08 +0800

    fix: correct class to className

--- index.jsx
+++ index.jsx
@@ -7,34 +7,34 @@ import "./index.css";
 function App() {
   return (
     <>
-      <nav class="py-6 px-4 bg-[#FFF7ED] flex items-center justify-between">
-        <Link to="/" class="uppercase font-bold text-2xl">
+      <nav className="py-6 px-4 bg-[#FFF7ED] flex items-center justify-between">
+        <Link to="/" className="uppercase font-bold text-2xl">
           #vanlife
         </Link>
-        <div class="font-semibold">
-          <Link to="/about" class="mr-4">
+        <div className="font-semibold">
+          <Link to="/about" className="mr-4">
             About
           </Link>
           <Link to="/vans">Vans</Link>
         </div>
       </nav>
-      <main class="bg-[url(./images/home-bg.png)] *:relative relative before:content-[''] before:absolute before:inset-0 before:block  before:bg-[rgba(0,0,0,0.46)] grow bg-cover bg-no-repeat text-white px-6">
-        <h1 class="text-2xl font-bold mt-16">
+      <main className="bg-[url(./images/home-bg.png)] *:relative relative before:content-[''] before:absolute before:inset-0 before:block  before:bg-[rgba(0,0,0,0.46)] grow bg-cover bg-no-repeat text-white px-6">
+        <h1 className="text-2xl font-bold mt-16">
           You got the travel plans, we got the travel vans.
         </h1>
-        <p class="mt-6">
+        <p className="mt-6">
           Add adventure to your life by joining the #vanlife movement. Rent the
           perfect van to make your perfect road trip.
         </p>
-        <button class="bg-[#FF8c38] mt-16 py-4 w-full text-center rounded hover:opacity-50">
+        <button className="bg-[#FF8c38] mt-16 py-4 w-full text-center rounded hover:opacity-50">
           Find Your van
         </button>
       </main>
-      <footer class="py-6 px-4 flex items-center justify-center bg-[#252525] text-[#AAA] font-semibold">
-        <p class="flex items-center">
-          <span class="text-2xl">&copy;</span>
+      <footer className="py-6 px-4 flex items-center justify-center bg-[#252525] text-[#AAA] font-semibold">
+        <p className="flex items-center">
+          <span className="text-2xl">&copy;</span>
           <span>2022</span>
-          <span class="uppercase">#valid</span>
+          <span className="uppercase">#valid</span>
         </p>
       </footer>
     </>
